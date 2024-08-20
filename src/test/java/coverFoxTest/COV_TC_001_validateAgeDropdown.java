d22d670eba79024d9ef286fa0f2bfcced6ab04a1
manishabhongal <bhongalmanisha@gmail.com>
2024-08-20 21:24:21 +0530

    [manisha] created new branch and added new TC

--- src/test/java/coverFoxTest/COV_TC_001_validateAgeDropdown.java
+++ src/test/java/coverFoxTest/COV_TC_001_validateAgeDropdown.java
@@ -1,9 +1,12 @@
 package coverFoxTest;
 
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class COV_TC_001_validateAgeDropdown {
   @Test
-  public void COV_TC_001_validateAgeDropdown() {
+  public void validateAgeDropdown() {
+	  
+	  Reporter.log("new changes done", true);
   }
 }
